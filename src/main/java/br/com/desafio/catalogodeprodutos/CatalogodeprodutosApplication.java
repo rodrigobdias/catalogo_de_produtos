3d4dc457aa5498e6c1a0ef7c62f2cec72e06fb91
rodrigobdias <rodrigobdias@gmail.com>
2021-02-26 15:03:10 -0300

    modeling of the 'Products' class

---
+++ src/main/java/br/com/desafio/catalogodeprodutos/CatalogodeprodutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.catalogodeprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogodeprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogodeprodutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/catalogodeprodutos/CatalogodeprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.catalogodeprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogodeprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
