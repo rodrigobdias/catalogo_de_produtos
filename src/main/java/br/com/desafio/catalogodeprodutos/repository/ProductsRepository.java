9a1f5df7a080a1f2cfefa7c8afa808afaa106c96
rodrigobdias <rodrigobdias@gmail.com>
2021-02-28 16:11:35 -0300

    development of 'ProductsRepository'

--- src/main/java/br/com/desafio/catalogodeprodutos/repository/ProductsRepository.java
+++ src/main/java/br/com/desafio/catalogodeprodutos/repository/ProductsRepository.java
@@ -1,9 +1,12 @@
 package br.com.desafio.catalogodeprodutos.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.desafio.catalogodeprodutos.model.Products;
 
 public interface ProductsRepository extends JpaRepository<Products, Long>{
 
+	Page<Products> findByName(String name, Pageable paginacao);
 }
