77bc6c2af05a8ab0311414b3bc784eaef00d38fb
rodrigobdias <rodrigobdias@gmail.com>
2021-02-28 16:17:34 -0300

    Construction of endpoints: 'POST /products', 'PUT /products/{id}', 'GET /products/{id}', 'GET /products' and 'DELETE /products/{id}'

--- src/main/java/br/com/desafio/catalogodeprodutos/CatalogodeprodutosApplication.java
+++ src/main/java/br/com/desafio/catalogodeprodutos/CatalogodeprodutosApplication.java
@@ -2,8 +2,10 @@ package br.com.desafio.catalogodeprodutos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CatalogodeprodutosApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/desafio/catalogodeprodutos/controller/ProductsController.java
+++ src/main/java/br/com/desafio/catalogodeprodutos/controller/ProductsController.java
@@ -1,13 +1,34 @@
 package br.com.desafio.catalogodeprodutos.controller;
 
-import java.util.List;
+import java.net.URI;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.desafio.catalogodeprodutos.controller.dto.DetailsProductDto;
 import br.com.desafio.catalogodeprodutos.controller.dto.ProductsDto;
+import br.com.desafio.catalogodeprodutos.controller.form.ProductsForm;
+import br.com.desafio.catalogodeprodutos.controller.form.UpdateProductsForm;
 import br.com.desafio.catalogodeprodutos.model.Products;
 import br.com.desafio.catalogodeprodutos.repository.ProductsRepository;
 
@@ -19,10 +40,89 @@ public class ProductsController {
 	private ProductsRepository productsRepository;
 	
 	@GetMapping
-	public List<ProductsDto> list() {
-		List<Products> products = productsRepository.findAll();
+	@Cacheable(value = "listProducts")
+	public Page<ProductsDto> list(@RequestParam(required = false) String name, 
+			@PageableDefault(sort = "id",  direction = Direction.DESC) Pageable paginacao) {
+		
+		if (name == null) {
+			Page<Products> products = productsRepository.findAll(paginacao);
+			return ProductsDto.convert(products);
+		} else {
+			Page<Products> products = productsRepository.findByName(name, paginacao);
+			return ProductsDto.convert(products);
+		}		
+	}
+	
 
-		return ProductsDto.convert(products);
+	
+//	@GetMapping("/search")
+//	public Page<ProductsDto> listSearch(String min_price) {
+//		
+//		if(min_price == null) {
+//			
+//		}
+//		Page<Products> products = productsRepository.findAll(paginacao);
+//
+//		return ProductsDto.convert(products);
+//		
+//	}	
+	
+	
+	@PostMapping
+	@Transactional
+	@CacheEvict(value = "listProducts", allEntries = true)
+	public ResponseEntity<ProductsDto> cadastrar(@RequestBody @Valid ProductsForm productsForm, UriComponentsBuilder uriBuilder) {
+		
+		Products products = productsForm.convert();
+		productsRepository.save(products);
+		
+		URI uri = uriBuilder.path("/products/{id}").buildAndExpand(products.getId()).toUri();
+		return ResponseEntity.created(uri).body(new ProductsDto(products));
+	}	
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<DetailsProductDto> detail(@PathVariable Long id) {
+		Optional<Products> products = productsRepository.findById(id);
+		
+		if(products.isPresent()) {
+			return ResponseEntity.ok(new DetailsProductDto(products.get()));			
+		}
+		
+		return ResponseEntity.notFound().build();
 		
 	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	@CacheEvict(value = "listProducts", allEntries = true)
+	public ResponseEntity<ProductsDto> update(@PathVariable Long id, @RequestBody @Valid UpdateProductsForm updateProductsForm) {
+		Optional<Products> optional = productsRepository.findById(id);
+		
+		if(optional.isPresent()) {
+			Products products = updateProductsForm.update(id, productsRepository);
+			
+			return ResponseEntity.ok(new ProductsDto(products));
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	@CacheEvict(value = "listProducts", allEntries = true)
+	public ResponseEntity<?> remove(@PathVariable Long id){
+		
+		Optional<Products> optional = productsRepository.findById(id);
+		
+		if(optional.isPresent()) {
+			productsRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+	}
+	
 }
