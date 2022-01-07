5190ad60080bb3ff21e2ee4a57ca5214635677e9
rodrigo <rodrigo@micro>
2022-01-06 22:09:25 -0300

    inclusão de 'ProductsMapper.java'

---
+++ src/main/java/br/com/desafio/catalogodeprodutos/controller/mapper/ProductsMapper.java
@@ -0,0 +1,13 @@
+package br.com.desafio.catalogodeprodutos.controller.mapper;
+
+import org.mapstruct.factory.Mappers;
+
+import br.com.desafio.catalogodeprodutos.controller.dto.ProductsDto;
+import br.com.desafio.catalogodeprodutos.model.Products;
+
+//@Mapper
+//public interface ProductsMapper {
+//
+//	ProductsMapper INSTANCE = Mappers.getMapper(ProductsMapper.class);
+//	ProductsDto toDto(Products products);
+//}
